6757fd4b078dea78ad26e48668e5842851ed6d44
Choi Kirin <choi.kirin@gmail.com>
2022-09-04 10:51:55 -0700

    Adopt list_able_to_map_from to Codeforces1721C (and time limit exceeded)

--- codeforces/1721/Codeforces1721C.java
+++ codeforces/1721/Codeforces1721C.java
@@ -43,6 +43,7 @@ public class Codeforces1721C {
             int size = sc.nextInt();
             int[] list_a = new int[size];
             int[] list_b = new int[size];
+            int[] list_able_to_map_from = new int[size];
             int[] list_d_min = new int[size];
             int[] list_d_max = new int[size];
             for(int j=0; j<size; ++j) {
@@ -52,45 +53,30 @@ public class Codeforces1721C {
                 list_b[j] = sc.nextInt();
             }
 
-            int prev_a = -1;
-            int prev_d = -1;
+            for (int idx_a=0; idx_a<list_a.length; ++idx_a) {
+                int idx_b = 0;
+                while (idx_b<list_b.length && list_a[idx_a] > list_b[idx_b]) {
+                    ++idx_b;
+                }
+                list_able_to_map_from[idx_a] = idx_b;
+            }
+
             for(int idx_a=0; idx_a<list_a.length; ++idx_a) {
-                if ( prev_a != -1 && prev_a == list_a[idx_a] ) {
-                    list_d_min[idx_a] = prev_d;
-                    continue;
+                list_d_min[idx_a] = list_b[list_able_to_map_from[idx_a]] - list_a[idx_a];
+                int idx_b = idx_a + 1;
+                while (idx_b<list_b.length && list_able_to_map_from[idx_b] < idx_b) {
+                    ++idx_b;
                 }
-                for(int idx_b=0; idx_b<list_b.length; ++idx_b) {
-                    if (list_b[idx_b] >= list_a[idx_a]) {
-                        list_d_min[idx_a] = list_b[idx_b] - list_a[idx_a];
-                        prev_a = list_a[idx_a];
-                        prev_d = list_d_min[idx_a];
-                        break;
-                    }
-                }
+                --idx_b;
+                list_d_max[idx_a] = list_b[idx_b] - list_a[idx_a];
             }
 
-            prev_a = -1;
-            prev_d = -1;
-            for(int idx_a=list_a.length-1; idx_a>=0; --idx_a) {
-                if ( prev_a != -1 && prev_a == list_a[idx_a] ) {
-                    list_d_max[idx_a] = prev_d;
-                    continue;
-                }
-                for(int idx_b=list_b.length-1; idx_b>=0; --idx_b) {
-                    if ( Codeforces1721C.ableToFill(list_a, list_b, list_a[idx_a], list_b[idx_b]) ) {
-                        list_d_max[idx_a] = list_b[idx_b] - list_a[idx_a];
-                        prev_a = list_a[idx_a];
-                        prev_d = list_d_max[idx_a];
-                        break;
-                    }
-                }
-
-            }
-
-            // Codeforces1721C.printList(list_a);
-            // Codeforces1721C.printList(list_b);
+            //Codeforces1721C.printList(list_a);
+            //Codeforces1721C.printList(list_b);
+            //Codeforces1721C.printList(list_able_to_map_from);
             Codeforces1721C.printList(list_d_min);
             Codeforces1721C.printList(list_d_max);
+            //System.out.println("----");
 
 
         }
